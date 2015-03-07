523bf58b359c65e93da7a001d4dc7d3ca1c2cfe3
klockett <kelockett@fullsail.edu>
2015-03-06 22:30:46 -0800

    goal 1
    var’s were created

---
+++ Wk1/goal1_assign_duel1/js/main.js
@@ -0,0 +1,32 @@
+/*
+Name:Kenlockett1
+Date: 3/6/15.
+Assignment:Goal1:Assignment: Duel 1
+ */
+
+// self-executing function
+(function(){
+
+
+console.log("FIGHT!!!");
+
+//Player name
+var playerOneName = "Spiderman";
+var playerTwoName = "Batma";
+
+//player damage
+var player1Damage = 20
+var playerTwoHealth = 100
+
+// player health
+var playerOneHealth = 100;
+var playerTwoHealth = 100;
+
+var round =1;
+
+
+
+
+
+
+})();
